b631723063b439a6b84ec79112770e5e8dc8a377
tsolakou <tsolakou@ics.forth.com>
2020-09-25 13:14:27 +0300

    First commit

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+function showSearch() {
+    if (document.getElementById("form").style.display === "none") {
+      document.getElementById("form").style.display = "block";
+    } else {
+      document.getElementById("form").style.display = "none";
+    }
+  }
+  
